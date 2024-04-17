96fe1647280f84e1930ce946755df1cee4f2f2d7
PArewa <bishal>
2024-04-18 04:34:16 +0545

    loaded model

--- request_handler.py
+++ request_handler.py
@@ -1,13 +1,39 @@
 import pickle
-def getPrompt(url):
-    
-    return url.replace('%20'," ")
-   
+import joblib
 
-def getResponse(url):
-    response = "";
-    data = {'key': 'value'} 
-    with open('rating.pkl', 'wb') as f:
-        pickle.dump(data, f)
+def getResponse():
+    rating_model = joblib.load('./models/rating.pkl');
+    price_model = joblib.load('./models/-price_hotel_owners_model.pkl');
+    rating_prdiction = rating_model.predict(input_data)
+    price_prediction = price_model.predict(X_train)
+    print(rating_prdiction)
 
-    return str(response)
+def test():
+    rating_model = joblib.load('./models/rating.pkl');
+    price_model = joblib.load('./models/-price_hotel_owners_model.pkl');
+    X_train = [
+        [1.0, 1.0, 2.0, 1.0, 2.0, 0, 48.858, 2.32125, 1.0, 8.627959388639653, 365.0, 0.0, 562.5]
+    ] 
+    input_data = {
+    1.0,
+    2.0,
+    3.0,
+    1.0,
+    40.7128,
+    -74.0060,
+    0.5,
+    rating.transform('Apartment'),
+    rating.transform('Entire home/apt'),
+    rating.transform('High'),
+    1.0,
+    2.0,
+    100.0,
+    50,
+    1.0,
+    365.0
+}
+    rating_prdiction = rating_model.predict(input_data)
+    # price_prediction = price_model.predict(X_train)
+    print(rating_prdiction)
+
+# test()
--- server.py
+++ server.py
@@ -2,13 +2,21 @@ from flask import request,Flask
 import flask
 from request_handler import getResponse
 
-app = Flask('D:\Backend')
+app = Flask('D:\\backend')
 
 @app.get
 @app.route('/')
 def controller():
-    prompt = request.args.get('text')
-    response = flask.Response(getResponse(prompt))
+    room = request.args.get('room', default=None, type=str)
+    property = request.args.get('property', default=None, type=str)
+    country = request.args.get('country', default=None, type=str)
+    amenities = request.args.get('amenities', default=0, type=int)
+    print("Hit 01")
+    print(room)
+    print(property)
+    print(country)
+    print(amenities)
+    response = flask.Response(getResponse(room))
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
